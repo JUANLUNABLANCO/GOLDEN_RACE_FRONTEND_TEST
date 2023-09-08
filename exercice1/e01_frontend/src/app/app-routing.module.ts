3761bf9f55faa59958c6fc9a4413a2533998a334
Jhony Moon <desarrolloaplicacionesweb.jmlb@gmail.com>
2023-09-08 18:36:38 +0200

    admin module with routing and lazy loading, unit testing ok

--- exercice1/e01_frontend/src/app/app-routing.module.ts
+++ exercice1/e01_frontend/src/app/app-routing.module.ts
@@ -2,7 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then( m => m.AdminModule)
+  },
+  // {
+  //   path: 'register',
+  //   // component: RegisterComponent
+  // },
+  // {
+  //   path: 'login',
+  //   // component: LoginComponent
+  // },
+  // {
+  //   path: 'users',
+  //   children:[
+  //     {
+  //       path: '',
+  //       // component: UsersComponent
+  //     },
+  //     {
+  //       path: ':id',
+  //       // component: UserProfileComponent
+  //     }
+  //   ]
+  // }
 ];
 
 @NgModule({
