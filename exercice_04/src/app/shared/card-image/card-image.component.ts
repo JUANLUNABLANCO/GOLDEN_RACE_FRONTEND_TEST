a96d2a1b354ed19fb11727a6e245265055f42327
JUANLUNABLANCO <desarrolloaplicacionesweb.jmlb@gmail.com>
2023-09-15 16:14:38 +0200

    exercice 04: layout mobile first, not responsive yet

--- exercice_04/src/app/shared/card-image/card-image.component.ts
+++ exercice_04/src/app/shared/card-image/card-image.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-image.component.less']
 })
 export class CardImageComponent {
-
+  cardImages = [
+    {
+      url: 'assets/images/image-a.png',
+      title: 'Imagen A',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget vulputate odio. Nullam at odio nec ex lacinia iaculis. Vivamus aliquet mi in nulla pharetra, a dignissim turpis hendrerit. Sed sollicitudin malesuada nisl, non pellentesque odio tincidunt vel. In hac habitasse platea dictumst. Fusce euismod sapien a quam vehicula, sed semper velit tincidunt. Aliquam erat volutpat. Maecenas a tincidunt mauris. Sed eleifend tincidunt leo, a dignissim felis. Donec bibendum lectus libero, nec malesuada libero facilisis ut. Cras ut rhoncus ligula. Nullam ac vestibulum ex. Eres bígamo wei! Praesent vehicula diam eu turpis aliquet, non tincidunt augue dictum. Vivamus at risus eget tortor fermentum laoreet. '
+    },
+    {
+      url: 'assets/images/image-b.png',
+      title: 'Imagen B',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget vulputate odio. Nullam at odio nec ex lacinia iaculis. Vivamus aliquet mi in nulla pharetra, a dignissim turpis hendrerit. Sed sollicitudin malesuada nisl, non pellentesque odio tincidunt vel. In hac habitasse platea dictumst. Fusce euismod sapien a quam vehicula, sed semper velit tincidunt. Aliquam erat volutpat. Maecenas a tincidunt mauris. Sed eleifend tincidunt leo, a dignissim felis. Donec bibendum lectus libero, nec malesuada libero facilisis ut. Cras ut rhoncus ligula. Nullam ac vestibulum ex. Eres bígamo wei! Praesent vehicula diam eu turpis aliquet, non tincidunt augue dictum. Vivamus at risus eget tortor fermentum laoreet. '
+    }
+  ]
 }
